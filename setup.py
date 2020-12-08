c1d4beed0ca32a8a582d9454cfbdf962ea1adc6c
Fraser <fraser.goldsworth@env-res.ox.ac.uk>
2020-12-08 09:27:40 +0000

    added

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+   name='llc_queue',
+   version='0.1.0',
+   author='Fraser Goldsworth',
+   author_email='fraser.goldsworth@physics.ox.ac.uk',
+   packages=['llc_queue', 'llc_queue.test'],
+   scripts=[],
+   #url='http://pypi.python.org/pypi/PackageName/',
+   license='LICENSE',
+   description='A package for managing the LLC queue',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "Django >= 1.1.1",
+       "pytest",
+   ],
+)
